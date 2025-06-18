c8303fdd597c97d6cc6ac3d1714ea65397201611
Danons <darulquro17@gmail.com>
2025-06-18 09:51:37 +0700

    commit

---
+++ application/logs/log-2025-06-18.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-06-18 09:36:17 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\_adminController.php 60
+ERROR - 2025-06-18 09:36:17 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\models\AuthModel.php 1819
+ERROR - 2025-06-18 09:36:17 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:38:29 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:38:29 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:41:16 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:41:16 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:41:38 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:41:38 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:43:12 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:43:12 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:43:58 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:43:58 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:44:02 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:44:02 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:44:06 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:44:06 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:44:09 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:45:20 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:45:20 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
+ERROR - 2025-06-18 09:45:40 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\_adminController.php 60
+ERROR - 2025-06-18 09:45:40 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\models\AuthModel.php 1819
+ERROR - 2025-06-18 09:45:40 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:45:47 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\controllers\panelbackend\Login.php 13
+ERROR - 2025-06-18 09:45:47 --> Severity: Warning --> Invalid argument supplied for foreach() D:\xampp\htdocs\egrc\application\core\CI_DB.php 322
